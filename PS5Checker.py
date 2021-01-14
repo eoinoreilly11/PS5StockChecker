4e4f736567ad40c194a2c8e2503cc03f44aece7a
Eoin O'Reilly <eoinoreilly11@gmail.com>
2021-01-14 02:28:11 +0000

    Update PS5Checker.py

--- PS5Checker.py
+++ PS5Checker.py
@@ -10,20 +10,8 @@ client = commands.Bot(command_prefix =  '!')
 async def on_ready():
     print("Bot Ready")
 
-TOKEN = 'Nzk4MjUxNDk1MjQwNzYxMzc1.X_yTow.Xjiy7EMesmlROk-p3nRBwVF9RTU'
+TOKEN = 'Nzk4MjUxNDk1MjQwNzYxMzc1.X_yTow.ywoTAZVSkY76K9A9PokUoNbg1cE'
 imgURL = 'https://i.guim.co.uk/img/media/f58aa676496e9eaba611000477f28d0232fd91eb/0_165_3378_2027/master/3378.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=e8c5338394932059caf20ba2516be828'
-
-#GameStop
-urlGS = 'https://www.gamestop.ie/PlayStation%205/Games/72504/playstation-5-console'
-content = requests.get(urlGS)
-soup = bs.BeautifulSoup(content.text, 'lxml')
-stockGS = soup.find("div", {"class": "bigBuyButtons SPNOpenMap"}).find('a').text
-
-#Smyths
-urlSmyths = 'https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259'
-content = requests.get(urlSmyths)
-soup = bs.BeautifulSoup(content.text, 'lxml')
-stockSmyths = str(soup.find("form", {"id": "customAddToCartForm"}).find('button'))
 
 @client.command(pass_context=True)
 async def go(ctx):
@@ -31,6 +19,18 @@ async def go(ctx):
 
     while 1:
         
+        #GameStop
+        urlGS = 'https://www.gamestop.ie/PlayStation%205/Games/72504/playstation-5-console'
+        content = requests.get(urlGS)
+        soup = bs.BeautifulSoup(content.text, 'lxml')
+        stockGS = soup.find("div", {"class": "bigBuyButtons SPNOpenMap"}).find('a').text
+
+        #Smyths
+        urlSmyths = 'https://www.smythstoys.com/ie/en-ie/video-games-and-tablets/playstation-5/playstation-5-consoles/playstation-5-console/p/191259'
+        content = requests.get(urlSmyths)
+        soup = bs.BeautifulSoup(content.text, 'lxml')
+        stockSmyths = str(soup.find("form", {"id": "customAddToCartForm"}).find('button'))
+
         if stockGS == 'Out Of Stock':
             print('GS Out Of Stock\n')
 
